d1ecf4ed66e734ef14d239890dbb188b83366998
EC2 Default User <ec2-user@ip-172-31-56-66.ec2.internal>
2020-10-14 12:36:55 +0000

    intial commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,45 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+  
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    @tweet = Tweet.new(message: params[:tweet][:message], tdate: Time.current)
+    if @tweet.save
+      flash[:notice] = '1レコード追加しました'
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    flash[:notice] = '1レコード削除しました'
+    redirect_to'/'
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.message = params[:tweet][:message]
+    if @tweet.save
+      flash[:notice] = '1レコード更新しました'
+      redirect_to '/'
+    else
+      render'edit'
+    end
+  end
+end
